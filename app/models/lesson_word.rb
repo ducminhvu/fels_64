fed478c12a1b441201b07f170e4cd4b3dfb0f1cd
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-07-01 10:24:03 +0700

    Update database

--- app/models/lesson_word.rb
+++ app/models/lesson_word.rb
@@ -1,4 +1,6 @@
 class LessonWord < ActiveRecord::Base
   belongs_to :lesson
   belongs_to :word
+
+  has_many :answers, through: :words
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,9 @@
 class Word < ActiveRecord::Base
   belongs_to :category
   has_many :lesson_words, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   validates :jp_word, presence: true
-  validates :correct_answer, presence: true, numericality: {only_integer: true}
 
   scope :located_in, ->category{where category_id: category.id}
 end
